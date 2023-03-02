b2f7100483d4569951c30c60c2a8e6089c14f4b1
Prakash Dalai <prakashd88@gmail.com>
2023-03-02 09:04:30 +0530

    story fix

--- src/routes/our-story/Story.svelte
+++ src/routes/our-story/Story.svelte
@@ -7,7 +7,7 @@
 			Our Story
 		</h1>
 	</div>
-	<center class="max-w-2xl pt-[45%] lg:mt-[10%] mx-auto px-4">
+	<center class="max-w-2xl pt-[45%] lg:pt-0 lg:mt-[10%] mx-auto px-4">
 		<h1 class="text-4xl text-neutral mb-5 font-bold font-Bodoni">Our Story</h1>
 		<p class="text-neutral lg:w-[35rem]">
 			SNCTM was founded in 2013 in Beverly Hills, California. From initial roots as a private
